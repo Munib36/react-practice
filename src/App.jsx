af9e76fdc7d568195592d9248b64207d0567d634
Munib36 <123148240+Munib36@users.noreply.github.com>
2024-01-16 15:59:31 +0000

    hi

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ export default function App() {
 	function allNewDice() {
 		if(tenzies){
 			setDiceArray(prevDice => {
-				return prevDice.map({ on: false, value: ranNum() });
+				return prevDice.map(x => ({ on: false, value: ranNum() }));
 			});
 			console.log("tenzies is true!!!")
 		}else{
